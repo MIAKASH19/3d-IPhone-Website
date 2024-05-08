da3a10be9be8302669b251bad37b7408cc8dec4d
MI AKASH <mszharul1990@gmail.com>
2024-05-08 23:37:21 +0600

    first commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
         <div className="w-[10vw] h-[10vw] rounded-full">
-          Loading...
+          Loading....
         </div>
       </div>
     </Html>
