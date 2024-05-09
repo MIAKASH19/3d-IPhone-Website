dd0c73fdcc9f862929900ba41f855eb17fc99d99
MI AKASH <mszharul1990@gmail.com>
2024-05-09 20:04:56 +0600

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from './components/Model'
 import * as Sentry from "@sentry/react"
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -13,6 +16,9 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-import { heroVideo, smallHeroVideo } from '../utils'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useEffect, useState } from 'react';
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
-  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo)
-
-  const handleVideoSrcSet = ()=>{
-    if (window.innerWidth < 768) {
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 760) {
       setVideoSrc(smallHeroVideo)
     } else {
       setVideoSrc(heroVideo)
@@ -16,35 +15,35 @@ const Hero = () => {
   }
 
   useEffect(() => {
-    window.addEventListener("resize", handleVideoSrcSet)
+    window.addEventListener('resize', handleVideoSrcSet);
+
     return () => {
-      window.removeEventListener("resize", handleVideoSrcSet)
+      window.removeEventListener('reisze', handleVideoSrcSet)
     }
   }, [])
-  
 
-  useGSAP(()=>{
-    gsap.to("#hero", {opacity: 1, duration: 1, delay: 2})
-    gsap.to("#cta", {opacity: 1, y: -50, duration: 1, delay: 2})
-  },[])
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 })
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 })
+  }, [])
 
   return (
-    <section className='w-full nav-height bg-black relative'>
-      <div className='h-5/6 flex-col flex-center'>
-        <p id='hero' className='hero-title'>iPhone 15 Pro</p>
-        <div className='md:w-10/12 w-9/12'>
-          <video className='pointer-events-none' autoPlay muted preload='auto' playsInline={true} key={videoSrc}>
-            <source src={videoSrc} type="video/mp4"/>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
       </div>
 
-      <div 
-      id='cta'
-      className='flex flex-col items-center opacity-0 translate-y-20'
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
       >
-        <a href="#highlights" className='btn'>Buy</a>
-        <p className='font-normal text-xl'>From $199/month or $999</p>
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   )
